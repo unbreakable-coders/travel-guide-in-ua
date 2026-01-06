3f3141d32b6d1cb1f63f22de941d731ee5d66c55
S-Khraban <S.Hraban@i.ua>
2026-01-06 13:49:25 +0200

    feat: add brand tokens and dev playground (#4)

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 import { router } from './router'
+
+import './styles/fonts.css'
+import './styles/tokens.css'
+import './style.css'
 
 createApp(App)
   .use(router)
